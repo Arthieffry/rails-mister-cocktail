f18c6e6c1fc22b755a9da9b630193d1e1dc51487
Arthur Thieffry <arthieffry@gmail.com>
2017-11-24 16:27:11 +0100

    Add des reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,30 @@
+class ReviewsController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @review.cocktail = @cocktail
+    @review.save
+    if @review.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
